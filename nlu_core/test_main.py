6292ba8789b524c5adb2c198e398830c68226ae3
Natu Pattarone <nataliapattarone@gmail.com>
2020-11-01 18:23:17 +0100

    NLU Core initial commit, also added dependencies needed, UTs for the Core class as well.

---
+++ nlu_core/test_main.py
@@ -0,0 +1,37 @@
+import unittest
+from nlu_core.main import Core
+
+
+class TestCoreMethods(unittest.TestCase):
+
+    def test_get_instance(self):
+        instance = Core()
+        self.assertTrue(instance is not None)
+
+    def test_predict_greet(self):
+        instance = Core()
+        prediction, probabilities = instance.predict_intent('Hi there!')
+
+        self.assertTrue(prediction is not None)
+        self.assertTrue(probabilities is not None)
+
+        self.assertTrue(prediction == 'greet')
+        self.assertEqual(len(probabilities), 3)
+
+    def test_predict_goodbye(self):
+        instance = Core()
+        prediction, probabilities = instance.predict_intent('see ya')
+
+        self.assertTrue(prediction is not None)
+        self.assertTrue(probabilities is not None)
+
+        self.assertTrue(prediction == 'goodbye')
+
+    def test_predict_restaurant_search(self):
+        instance = Core()
+        prediction, probabilities = instance.predict_intent('Where can I find a french restaurant?')
+
+        self.assertTrue(prediction is not None)
+        self.assertTrue(probabilities is not None)
+
+        self.assertTrue(prediction == 'restaurant_search')
