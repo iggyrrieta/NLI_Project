ca58ec299f7cb9b82d0875ff7ca0a119cb35b02b
Sayan Goswami <goswami.sayan47@gmail.com>
2020-12-12 15:30:49 +0530

    Add changes.

--- dm_core/ct_restaurant.py
+++ dm_core/ct_restaurant.py
@@ -86,9 +86,48 @@ class ConversationTracker:
         self.last_input = text
         self.last_entity = [i.text for i in entities]
 
-        # if self.next_agent_action_type == 'request':
-        #     self._get_city_id(self.last_entity[0])
-        #     self._get_nearby_resturants(self.last_entity[0])
+        logger.info(f"Entities: {self.last_entity}")
+
+        popular_cuisines = list(map(lambda x: x.lower(), [
+            "Ainu", "Albanian", "Argentine", "Andhra", "American", "Anglo-Indian", "Arab", "Armenian", "Assyrian",
+            "Awadhi", "Azerbaijani", "Balochi", "Belarusian", "Bangladeshi", "Bengali", "Berber", "Brazilian",
+            "British", "Buddhist", "Bulgarian", "Cajun", "Cantonese", "Caribbean", "Chechen", "Chinese",
+            "Chinese Islamic", "Circassian", "Crimean Tatar", "Cypriot", "Czech", "Danish", "Egyptian", "English",
+            "Ethiopian", "Eritrean", "Estonian", "French", "Filipino", "Georgian", "German", "Goan", "Goan Catholic",
+            "Greek", "Gujarati", "Hyderabad", "Indian", "Indian Chinese", "Indian Singaporean",
+            "Indonesian", "Inuit", "Irish", "Italian-American", "Italian", "Jamaican", "Japanese", "Jewish",
+            "Karnataka", "Kazakh", "Keralite", "Korean", "Kurdish", "Laotian", "Lebanese", "Latvian", "Lithuanian",
+            "Louisiana Creole", "Maharashtrian", "Mangalorean", "Malay", "Malaysian Chinese",
+            "Malaysian Indian", "Mediterranean", "Mennonite", "Mexican", "Mordovian", "Mughal",
+            "Native American", "Nepalese", "New Mexican", "Odia", "Parsi", "Pashtun", "Polish", "Pennsylvania Dutch",
+            "Pakistani", "Peranakan", "Persian", "Peruvian", "Portuguese", "Punjabi", "Québécois", "Rajasthani",
+            "Romanian", "Russian", "Sami", "Serbian", "Sindhi", "Slovak", "Slovenian", "Somali", "South Indian",
+            "Soviet", "Spanish", "Sri Lankan", "Taiwanese", "Tatar", "Texan", "Thai", "Turkish", "Tamil", "Udupi",
+            "Ukrainian", "Vietnamese", "Yamal", "Zambian", "Zanzibari"]))
+
+        # Fill cuisine slot
+        t_ent = self.last_entity
+        found_cuisine = False
+        found_location = False
+
+        for i, e in enumerate(t_ent):
+            if e.lower() in popular_cuisines:
+                self.agent_actions[1]['cuisine'] = e.lower()
+                t_ent.pop(i)
+                found_cuisine = True
+
+            if found_cuisine:
+                break
+
+        if len(t_ent) > 0:
+            self.agent_actions[0]['location'] = t_ent[0]
+            found_location = True
+
+        if found_location and found_cuisine:
+            self.next_agent_action_type = 'inform'
+
+        logger.info(f"Slots: {self.agent_actions}")
+
 
         # Increment utterance id.
         self._id += 1
@@ -101,8 +140,6 @@ class ConversationTracker:
         '''Publish all processed info
            to be used by dm subscribe
         '''
-
-        logger.info(f"Entities: {self.last_entity}")
 
         # slot not detected
         if self.next_agent_action_type == 'request':
@@ -123,14 +160,37 @@ class ConversationTracker:
 
             if self.agent_actions[0]['location'] == '':
                 self.agent_actions[0]['location'] = self.last_entity[0]
-                self.place = self.last_entity[0]
+                self.place = self.agent_actions[0]['location']
+
                 self.next_agent_action = restaurant_nlg.inform_place.format(place=self.place)
                 self.next_agent_action += restaurant_nlg.request_cuisine
                 self.next_agent_action_type = 'inform'
             # Next step, look for the other slot
             elif self.agent_actions[1]['cuisine'] == '':
                 self.agent_actions[1]['cuisine'] = self.last_entity[0]
-                self.cuisine = self.last_entity[0]
+                self.cuisine = self.agent_actions[1]['cuisine']
+
+                query = f"{self.cuisine} restaurants near {self.place}"
+                logger.info(f"Finding nearby restaurants. Search term: {query}")
+                self._get_nearby_restaurants(query)
+
+                name = self.nearby_restaurant["name"]
+                place = self.nearby_restaurant["formatted_address"]
+
+                if self.nearby_restaurant["opening_hours"]["open_now"]:
+                    open = "open"
+                else:
+                    open = "closed"
+
+                self.next_agent_action = restaurant_nlg.inform_restaurant.format(
+                                            name=name,
+                                            place=place,
+                                            open=open)
+                self.next_agent_action_type = 'request'
+            # All slots present, proceed with query.
+            else:
+                self.place = self.agent_actions[0]['location']
+                self.cuisine = self.agent_actions[1]['cuisine']
 
                 query = f"{self.cuisine} restaurants near {self.place}"
                 logger.info(f"Finding nearby restaurants. Search term: {query}")
